778f808096ae6309f2cfa8e715e67585304876d5
Mateus Honorato <mateush.honorato@gmail.com>
2019-10-08 03:08:12 -0300

    test

---
+++ src/NumberButtons/styles.js
@@ -0,0 +1,33 @@
+const React = require('react-native');
+const { StyleSheet } = React;
+
+export default {
+
+  container: {
+    flex: 1
+  },
+
+  txtDefault: {
+    color: '#000',
+    fontSize: 20
+  },
+
+  contRow: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingBottom: 5
+  },
+
+  contButton: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius:10,
+    backgroundColor: '#9bd8ef',
+    marginRight:5,
+    marginLeft:5,
+    marginTop: 5,
+    paddingTop: 5,
+    paddingBottom: 5
+  }
+};
